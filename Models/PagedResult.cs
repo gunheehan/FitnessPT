3459bfa3096ebd4dbcf193a574fb333ccd72f485
GEONHEEHAN <gksrjsgml13@naver.com>
2025-10-27 22:39:19 +0900

    PagedResult.cs 통신 파람 수정

--- Models/PagedResult.cs
+++ Models/PagedResult.cs
@@ -1,10 +1,13 @@
 namespace FitnessPT.Models;
 
+[Serializable]
 public class PagedResult<T>
 {
-    public List<T> Items { get; set; } = new();
+    public List<T> Items { get; set; }
     public int Page { get; set; }
     public int PageSize { get; set; }
     public int TotalCount { get; set; }
     public int TotalPages { get; set; }
+    public bool HasPrevious => Page > 1;
+    public bool HasNext => Page < TotalPages;
 }
